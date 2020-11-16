f8da2e4db14967a0c40c6ffbab2f8c2a746100e9
Slackjvo <slackjvo@gmail.com>
2020-11-16 09:43:44 +0100

    id param mongo changed in api

--- routers/fish.js
+++ routers/fish.js
@@ -10,32 +10,34 @@ router.get('/fishes', async (req, res) => {
             return res.status(404).send()
         }
 
-        res.send(fishes)
+        return res.send(fishes)
     } catch (e) {
-        res.status(500).send()
+        return res.status(500).send()
     }
 })
 
 router.get('/fishes/:id', async (req, res) => {
     try {
-        const fish = await Fish.find({"id":req.params.id})
+        const fish = await Fish.findById(req.params.id)
         if (!fish) return res.status(404).send()
-        res.send(fish)
+        return res.send(fish)
     } catch (e) {
-        res.status(500).send()
+        return res.status(500).send()
     }
 })
 
 router.post('/fishes', async (req, res) => {
-	const oldFish = await Fish.find({"id":req.body['id']})
-	if(oldFish.length > 0) res.send('Fish already exists')
-	const fish = new Fish(req.body)
-    try {
-        await fish.save()
-        res.status(201).send('Fish Added')
-    } catch (e){
-        res.status(400).send(e)
-    }
+	try {
+		const oldFish = await Fish.find({"id":req.body['id']})
+		console.log(oldFish)
+		if(oldFish.length > 0)	return res.send('Fish already exists')
+		const fish = new Fish(req.body)
+		await fish.save()
+		res.status(201).send('Fish Added')
+	} catch (e){
+		return res.status(400).send(e)
+	}
+	
 })
 
 router.patch('/fishes/:id', async (req, res) => {
@@ -45,21 +47,21 @@ router.patch('/fishes/:id', async (req, res) => {
 
     if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
 	try{
-        const fish = await Fish.findOneAndUpdate({"id":req.params.id},req.body)
-		if (!fish) res.status(404).send()
-        res.send(fish)
+        const fish = await Fish.findByIdAndUpdate(req.params.id,req.body)
+		if (!fish) return res.status(404).send()
+        return res.send(fish)
 	}catch{
-		res.status(500).send()
+		return res.status(500).send()
 	}
 })
 
 router.delete('/fishes/:id', async (req, res) => {
     try {
-        const fish = await Fish.findOneAndDelete({"id":req.params.id})
-        if (!fish) res.status(404).send()
-        res.send(fish)
+        const fish = await Fish.findByIdAndDelete(req.params.id)
+        if (!fish) return res.status(404).send()
+        return res.send(fish)
     } catch (e) {
-        res.status(500).send()
+        return res.status(500).send()
     }
 })
 
